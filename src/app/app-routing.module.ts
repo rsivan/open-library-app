ac491e36799bda595b0e460156a877ee03b8537a
Reuben Sivan <rsivan@sivansoftware.com>
2020-01-07 16:28:40 -0800

    delete home and list page stubs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,14 +8,6 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
-    path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  },
-  {
     path: 'subjects',
     loadChildren: () => import('./pages/subjects-browse/subjects-browse.module').then( m => m.SubjectsBrowsePageModule)
   },
